cf3bff278b4ed483c09e627d20453129fefe2e59
Leonardo Silva <s2leoj@gmail.com>
2023-10-14 00:53:08 -0300

    Add player camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿// CameraFollow: Smoothly moves the attached camera to follow a target object,
+// adjusting its position based on the provided offset and speed.
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public FloatReference speed;
+    public Vector3Variable offset;
+
+    private Vector3 targetPos;
+
+    private void Update()
+    {
+        if (target == null)
+            return;
+
+        targetPos = target.position + offset.value;
+        transform.position = Vector3.Slerp(transform.position, targetPos, speed.Value * Time.deltaTime);
+    }
+}
