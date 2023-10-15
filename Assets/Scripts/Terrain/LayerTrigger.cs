1135783c22c8a9b20007d6264164d0e8c51b1289
Leonardo Silva <s2leoj@gmail.com>
2023-10-14 23:32:01 -0300

    Add layer trigger

---
+++ Assets/Scripts/Terrain/LayerTrigger.cs
@@ -0,0 +1,22 @@
+﻿//Dynamically adjust the layer and sorting layer properties of GameObjects
+// when they exit a designated trigger area.
+using UnityEngine;
+
+public class LayerTrigger : MonoBehaviour
+{
+    public string layer;
+    public string sortingLayer;
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        other.gameObject.layer = LayerMask.NameToLayer(layer);
+
+        other.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = sortingLayer;
+        SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer sr in srs)
+        {
+            sr.sortingLayerName = sortingLayer;
+        }
+    }
+
+}
