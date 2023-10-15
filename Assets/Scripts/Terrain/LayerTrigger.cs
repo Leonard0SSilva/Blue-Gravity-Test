9627989b6b0a99bc3b2b73213e4e8c5d98c7573d
Leonardo Silva <s2leoj@gmail.com>
2023-10-15 03:55:17 -0300

    Fix layer trigger enabling inactive gameObjects

--- Assets/Scripts/Terrain/LayerTrigger.cs
+++ Assets/Scripts/Terrain/LayerTrigger.cs
@@ -12,7 +12,7 @@ public class LayerTrigger : MonoBehaviour
         other.gameObject.layer = LayerMask.NameToLayer(layer);
 
         other.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = sortingLayer;
-        SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+        SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>(true);
         foreach (SpriteRenderer sr in srs)
         {
             sr.sortingLayerName = sortingLayer;
